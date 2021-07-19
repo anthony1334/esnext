276872bf021e6ee11d8d8c2bc1538ac69cd67e33
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-07-19 13:59:49 +0200

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let
+let favoriteCityId = "rome";
+console.log(favoriteCityId);
+favoriteCityId ="paris";
+console.log(favoriteCityId);
+const citiesId =["paris", "nyc", "rome", "rio-de-janeiro"];
+console.log(citiesId);
+citiesId =[];
+console.log(citiesId);
+//TypeError: Assignment to constant variable.
+citiesId.push("tokyo");
+console.log(citiesId);
